3bd3bd2c27104f3a68c38d2cb599d2acf7d10a74
gammazero <gammazero@users.noreply.github.com>
2023-05-31 02:23:29 -0700

    Remove support for libp2p find client
    Part of fix for https://github.com/ipni/storetheindex/issues/1842

---
+++ find/client/http/old_client.go
@@ -0,0 +1,20 @@
+package httpclient
+
+import (
+	"github.com/ipni/go-libipni/find/client"
+)
+
+type Client = client.Client
+type Option = client.Option
+
+// Deprecated: Use github.com/ipni/go-libipni/find/client
+func New(baseURL string, options ...client.Option) (*client.Client, error) {
+	return client.New(baseURL, options...)
+}
+
+type DHashClient = client.DHashClient
+
+// Deprecated: Use github.com/ipni/go-libipni/find/client
+func NewDHashClient(dhstoreURL, stiURL string, options ...client.Option) (*client.DHashClient, error) {
+	return client.NewDHashClient(dhstoreURL, stiURL, options...)
+}
