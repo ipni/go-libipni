a12f29ece6f06613338365f6b7f377268a0b77a1
gammazero <gammazero@users.noreply.github.com>
2023-07-17 12:09:08 -0700

    Replace lookup package with rwriter package

---
+++ rwriter/response_writer.go
@@ -0,0 +1,177 @@
+package rwriter
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"mime"
+	"net/http"
+	"path"
+	"strings"
+
+	"github.com/ipfs/go-cid"
+	"github.com/ipni/go-libipni/apierror"
+	"github.com/mr-tron/base58"
+	"github.com/multiformats/go-multihash"
+)
+
+const (
+	mediaTypeNDJson = "application/x-ndjson"
+	mediaTypeJson   = "application/json"
+	mediaTypeAny    = "*/*"
+)
+
+type ResponseWriter struct {
+	w          http.ResponseWriter
+	f          http.Flusher
+	encoder    *json.Encoder
+	mh         multihash.Multihash
+	mhCode     uint64
+	nd         bool
+	pathType   string
+	preferJson bool
+	status     int
+}
+
+func New(w http.ResponseWriter, r *http.Request, preferJson bool) (ResponseWriter, error) {
+	accepts := r.Header.Values("Accept")
+	var nd, okJson bool
+	for _, accept := range accepts {
+		amts := strings.Split(accept, ",")
+		for _, amt := range amts {
+			mt, _, err := mime.ParseMediaType(amt)
+			if err != nil {
+				return ResponseWriter{}, apierror.New(errors.New("invalid Accept header"), http.StatusBadRequest)
+			}
+			switch mt {
+			case mediaTypeNDJson:
+				nd = true
+			case mediaTypeJson:
+				okJson = true
+			case mediaTypeAny:
+				nd = !preferJson
+				okJson = true
+			}
+			if nd && okJson {
+				break
+			}
+		}
+	}
+
+	if len(accepts) == 0 {
+		if !preferJson {
+			// If there is no `Accept` header and JSON is preferred then be
+			// forgiving and fall back onto JSON media type. Otherwise,
+			// strictly require `Accept` header.
+			return ResponseWriter{}, apierror.New(errors.New("accept header must be specified"), http.StatusBadRequest)
+		}
+	} else if !okJson && !nd {
+		return ResponseWriter{}, apierror.New(fmt.Errorf("media type not supported: %s", accepts), http.StatusBadRequest)
+	}
+
+	var b []byte
+	pathType := path.Base(path.Dir(r.URL.Path))
+	switch pathType {
+	case "multihash":
+		var err error
+		b, err = base58.Decode(strings.TrimSpace(path.Base(r.URL.Path)))
+		if err != nil {
+			return ResponseWriter{}, apierror.New(multihash.ErrInvalidMultihash, http.StatusBadRequest)
+		}
+	case "cid":
+		c, err := cid.Decode(strings.TrimSpace(path.Base(r.URL.Path)))
+		if err != nil {
+			return ResponseWriter{}, apierror.New(err, http.StatusBadRequest)
+		}
+		b = c.Hash()
+	default:
+		return ResponseWriter{}, apierror.New(errors.New("unsupported resource type"), http.StatusBadRequest)
+	}
+
+	dm, err := multihash.Decode(b)
+	if err != nil {
+		return ResponseWriter{}, apierror.New(err, http.StatusBadRequest)
+	}
+
+	flusher, _ := w.(http.Flusher)
+	if nd {
+		w.Header().Set("Content-Type", mediaTypeNDJson)
+		w.Header().Set("Connection", "Keep-Alive")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+	} else {
+		w.Header().Set("Content-Type", mediaTypeJson)
+	}
+
+	return ResponseWriter{
+		w:          w,
+		f:          flusher,
+		encoder:    json.NewEncoder(w),
+		mh:         multihash.Multihash(b),
+		mhCode:     dm.Code,
+		nd:         nd,
+		pathType:   pathType,
+		preferJson: preferJson,
+		status:     http.StatusOK,
+	}, nil
+}
+
+func (w *ResponseWriter) Multihash() multihash.Multihash {
+	return w.mh
+}
+
+func (w *ResponseWriter) MultihashCode() uint64 {
+	return w.mhCode
+}
+
+func (w *ResponseWriter) IsND() bool {
+	return w.nd
+}
+
+func (w *ResponseWriter) PathType() string {
+	return w.pathType
+}
+
+func (w *ResponseWriter) WriteND() error {
+	_, err := w.w.Write([]byte("\n"))
+	if err != nil {
+		return err
+	}
+	if w.f != nil {
+		w.f.Flush()
+	}
+	return nil
+}
+
+func (w *ResponseWriter) Encoder() *json.Encoder {
+	return w.encoder
+}
+
+func (w *ResponseWriter) Header() http.Header {
+	return w.w.Header()
+}
+
+func (w *ResponseWriter) Write(b []byte) (int, error) {
+	return w.w.Write(b)
+}
+
+func (w *ResponseWriter) WriteHeader(code int) {
+	w.status = code
+	w.w.WriteHeader(code)
+}
+
+func (w *ResponseWriter) Status() int {
+	return w.status
+}
+
+func MatchQueryParam(r *http.Request, key, value string) (bool, bool) {
+	labels, present := r.URL.Query()[key]
+	if !present {
+		return false, false
+	}
+	for _, label := range labels {
+		if label == value {
+			return true, true
+		}
+	}
+	return true, false
+}
