52811ac025c07326326fba6addbee739be508cbd
gammazero <gammazero@users.noreply.github.com>
2023-10-02 12:16:06 -0700

    correct schema doc

--- ingest/schema/doc.go
+++ ingest/schema/doc.go
@@ -1,7 +1,7 @@
-// Package schema defines the makeup of an advertisement.  An advertisement is
+// Package schema defines the makeup of an advertisement. An advertisement is
 // used to inform an indexer that there are updates to a provider's content.
-// The IsRm and Entries fields determine how the indexer handles
-// the advertisement. The following table describes this:
+// The IsRm and Entries fields determine how the indexer handles the
+// advertisement, as described by the following table:
 //
 //	IsRm    Entries    Action
 //
@@ -9,14 +9,13 @@
 // false | NoEntries | Update metadata
 // false | data      | Update metadata and index entries
 // true  | NoEntries | Delete content with context ID
-// true  | data      | Delete specific multihash indexes *
+// true  | data      | Delete content with context ID (ignore entries)
 //
-// * Deleting entries still requires a context ID, because a multihash can map
-// to multiple contextID/metadata values.
+// When removing content (IsRm true) the metadata and entries are ignored. An
+// advertisement that removes content cannot have extended provider
+// information.
 //
-// When removing content (IsRm true) the metadata is ignored.
-//
-// All advertisements update the provider's addresses.  To create an
+// All advertisements update the provider's addresses. To create an
 // advertisement that only updates a provider's address, create an
-// advertisement to remove content using a context ID that is not used.
+// advertisement that contains no metadata and no entries.
 package schema
